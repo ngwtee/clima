867a887cd66486bfee33788358a1d71113587513
Ngoni <ngonidzashegwt@gmail.com>
2021-04-27 11:30:20 +0200

    minimal code to get location using geolocator API

---
+++ android/app/src/main/kotlin/ngonigwata/clima/MainActivity.kt
@@ -0,0 +1,6 @@
+package ngonigwata.clima
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+ class _LoadingScreenState extends State<LoadingScreen> {
+ @override
+  void initState() {
+   getLocation();
+    super.initState();
+  }
+
+  void getLocation() async{
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+    //print('$position.altitude and $position.latitude' );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
